93b3b5065b375660f735d42b8eb924bf53bc0ffb
PriyanshuSha <priyanshusharma90284@gmail.com>
2022-10-13 22:07:06 +0530

    first day

---
+++ src/main/java/com/springCore/school.java
@@ -0,0 +1,20 @@
+package com.springCore;
+
+public class school {
+	private schoolClass Class;
+	
+	public schoolClass getSchCl() {
+		return Class;
+	}
+
+	public void setSchCl(schoolClass schCl) {
+		this.Class = schCl;
+	}
+
+	public void greeting() {
+		
+		System.out.println("welcome to school ");
+		Class.classs();
+	}
+
+}
---
+++ src/main/java/com/springCore/schoolClass.java
@@ -0,0 +1,11 @@
+package com.springCore;
+
+public class schoolClass {
+	
+	public void classs() {
+		
+		System.out.println("welcome to class ");
+		
+	}
+
+}
